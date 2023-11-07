00a40496d656eddc1cc58e19b0eea0e8c68b5b50
Luciano Malvicino <luchothelion@gmail.com>
2023-11-07 13:51:08 -0500

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000; // Use the PORT environment variable if set, or default to 3000
+
+// Define routes and middleware here
+app.get('/', (req, res) => {
+  res.send('Hello, Worldey!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
