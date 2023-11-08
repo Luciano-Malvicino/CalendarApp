09e54b82f47d73355fa01b0a567a01d91a8a8de4
Luciano Malvicino <luchothelion@gmail.com>
2023-11-08 12:54:37 -0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000; // Use the PORT environment variable if s
 
 // Define routes and middleware here
 app.get('/', (req, res) => {
-  res.send('Hello, Worldeyasdasdaseeeaa!');
+  res.send('Hello, World!');
 });
 
 // Start the server
