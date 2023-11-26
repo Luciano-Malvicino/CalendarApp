ec7e2570e1f7ad3e81976db48781d8b96743f113
Luciano Malvicino <luchothelion@gmail.com>
2023-11-26 16:30:50 -0500

    more work on UserSaves

--- frontend/src/components/UserSaves/usersaves.jsx
+++ frontend/src/components/UserSaves/usersaves.jsx
@@ -3,58 +3,48 @@ import SideNav from '../SideNav/SideNav';
 import ContextMenu from '../ContextMenu/ContextMenu';
 import './UserSaves.css'
 
+function addSave(event) {
+  const parentElement = event.currentTarget.parentNode.parentNode;
+  console.log('Parent element:', parentElement);
+}
+
 function UserSaves() {
 
   return (
     <div>
       <SideNav id="save" />
       <div className='user-save-wrapper'>
-        <p className='user-save-title'>Common Saves</p>
+        <p className='user-save-title'>User Saves</p>
         <div className='user-wrapper'>
           <div id='metroid' className='user-save-table'>
-            <p className='user-table-title'>Metroid Fusion</p>
+            <div className='title-wrapper'>
+              <p className='user-table-title'>Metroid Fusion</p>
+              <img className='add-svg' src='/src/assets/plus.svg' onClick={ addSave }/>
+            </div>
             <hr className='user-save-line'/>
-            <div className='user-save-a'>
-              <p className='save-table-content'>At Final Fight</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
-            <div className='user-save-a'>
-              <p className='save-table-content'>Save File 100%</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
+            {/* this is where you shove the files */}
           </div>
           <div id='pokemon' className='user-save-table'>
-            <p className='user-table-title'>Pokemon Emerald</p>
+            <div className='title-wrapper'>
+              <p className='user-table-title'>Pokemon Emerald</p>
+              <img className='add-svg' src='/src/assets/plus.svg'/>
+            </div>
             <hr className='user-save-line'/>
-            <div className='user-save-a'>
-              <p className='save-table-content'>Shiny Living Dex + 100%</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
-            <div className='user-save-a'>
-              <p className='save-table-content'>At Victory Road</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
+            {/* this is where you shove the files */}
           </div>
           <div id='mario' className='user-save-table'>
-            <p className='user-table-title'>Super Mario Bros Advanced 4</p>
-            <hr className='user-save-line'/>
-            <div className='user-save-a'>
-              <p className='save-table-content'>Save File 100%</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
+            <div className='title-wrapper'>
+              <p className='user-table-title'>Super Mario Bros Advanced 4</p>
+              <img className='add-svg' src='/src/assets/plus.svg'/>
             </div>
+            <hr className='user-save-line'/>
+            {/* this is where you shove the files */}
           </div>
           <div id='mariok' className='user-save-table'>
-            <p className='user-table-title'>Mario Kart Super Circuit</p>
+            <div className='title-wrapper'>
+              <p className='user-table-title'>Mario Kart Super Circuit</p>
+              <img className='add-svg' src='/src/assets/plus.svg'/>
+            </div>
             <hr className='user-save-line'/>
             <div className='user-save-a'>
               <p className='save-table-content'>50 + 100CC Finished</p>
@@ -62,28 +52,14 @@ function UserSaves() {
                 <ContextMenu />
               </div>
             </div>
-            <div className='user-save-a'>
-              <p className='save-table-content'>All Tracks Unlocked</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
-            <div className='user-save-a'>
-              <p className='save-table-content'>Save File 100%</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
-            </div>
           </div>
           <div id='zelda' className='user-save-table'>
-            <p className='user-table-title'>The Legend of Zelda</p>
-            <hr className='user-save-line'/>
-            <div className='user-save-a'>
-              <p className='save-table-content'>Save File 100%</p>
-              <div className='context-menu-div'>
-                <ContextMenu />
-              </div>
+            <div className='title-wrapper'>
+              <p className='user-table-title'>The Legend of Zelda</p>
+              <img className='add-svg' src='/src/assets/plus.svg'/>
             </div>
+            <hr className='user-save-line'/>
+            {/* this is where you shove the files */}
           </div>
         </div>
       </div>
