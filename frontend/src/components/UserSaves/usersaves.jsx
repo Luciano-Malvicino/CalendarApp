6d1308c47932e457c7b61204bd5109f55526c489
Aaron Moreno <aa054064@ucf.edu>
2023-11-26 14:16:16 -0600

    S3 Bucket Backend

--- frontend/src/components/UserSaves/usersaves.jsx
+++ frontend/src/components/UserSaves/usersaves.jsx
@@ -1,32 +1,94 @@
 import React from 'react';
-import './usersaves.css';
-import PlusSvg from '../../assets/usersave-plus.svg'; // Importing SVG as a React component
-import OptionsImage from '../../assets/options.png'; // Importing image
+import SideNav from '../SideNav/SideNav';
+import ContextMenu from '../ContextMenu/ContextMenu';
+import './UserSaves.css'
 
 function UserSaves() {
 
-    const maxNameLength = 10;
-  
-    return (
-        <div className = "parent-all">
-            <div className="usersave-parent-title">
-                <h1 className="usersave-title">User Saves</h1>
+  return (
+    <div>
+      <SideNav id="save" />
+      <div className='user-save-wrapper'>
+        <p className='user-save-title'>Common Saves</p>
+        <div className='user-wrapper'>
+          <div id='metroid' className='user-save-table'>
+            <p className='user-table-title'>Metroid Fusion</p>
+            <hr className='user-save-line'/>
+            <div className='user-save-a'>
+              <p className='save-table-content'>At Final Fight</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
             </div>
-            <table>
-                <tr>
-                    <th colspan = "2" >Mario Bros 3</th>
-                </tr>
-                <tr>
-                    <td className = "name" >Peter</td>
-                    <td className = "option">Griffin</td>
-                </tr>
-                <tr>
-                    <td>Lois</td>
-                    <td>Griffin</td>
-                </tr>
-            </table>
+            <div className='user-save-a'>
+              <p className='save-table-content'>Save File 100%</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+          </div>
+          <div id='pokemon' className='user-save-table'>
+            <p className='user-table-title'>Pokemon Emerald</p>
+            <hr className='user-save-line'/>
+            <div className='user-save-a'>
+              <p className='save-table-content'>Shiny Living Dex + 100%</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+            <div className='user-save-a'>
+              <p className='save-table-content'>At Victory Road</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+          </div>
+          <div id='mario' className='user-save-table'>
+            <p className='user-table-title'>Super Mario Bros Advanced 4</p>
+            <hr className='user-save-line'/>
+            <div className='user-save-a'>
+              <p className='save-table-content'>Save File 100%</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+          </div>
+          <div id='mariok' className='user-save-table'>
+            <p className='user-table-title'>Mario Kart Super Circuit</p>
+            <hr className='user-save-line'/>
+            <div className='user-save-a'>
+              <p className='save-table-content'>50 + 100CC Finished</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+            <div className='user-save-a'>
+              <p className='save-table-content'>All Tracks Unlocked</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+            <div className='user-save-a'>
+              <p className='save-table-content'>Save File 100%</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+          </div>
+          <div id='zelda' className='user-save-table'>
+            <p className='user-table-title'>The Legend of Zelda</p>
+            <hr className='user-save-line'/>
+            <div className='user-save-a'>
+              <p className='save-table-content'>Save File 100%</p>
+              <div className='context-menu-div'>
+                <ContextMenu />
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default UserSaves;
